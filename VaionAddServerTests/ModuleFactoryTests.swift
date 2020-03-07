5b6e07c2921e04cb73891c9f20037f348ad8d5bd
Murat Sudan <tarumnadus@gmail.com>
2020-03-07 01:17:59 +0000

    Refactored ModuleFactory

---
+++ VaionAddServerTests/ModuleFactoryTests.swift
@@ -0,0 +1,19 @@
+//
+//  ModuleFactoryTests.swift
+//  VaionAddServerTests
+//
+//  Created by Murat Sudan on 7.03.2020.
+//  Copyright © 2020 Tarum Nadus. All rights reserved.
+//
+
+import XCTest
+@testable import VaionAddServer
+
+class ModuleFactoryTests: XCTestCase {
+    
+    func testModuleFactoryMakesAddServerModule() {
+        let (vc, presenter) = ModuleFactory().makeAddServerModule()
+        XCTAssertTrue(vc is AddServerViewController)
+        XCTAssertTrue(presenter is AddServerPresenter)
+    }
+}
