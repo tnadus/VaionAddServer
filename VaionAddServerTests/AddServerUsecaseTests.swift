401a8e35ef036c098dc9109ee5eac96799996ae0
Murat Sudan <tarumnadus@gmail.com>
2020-03-08 09:05:25 +0000

    Removed unused setup and teardown from AddServerUsecaseTests

--- VaionAddServerTests/AddServerUsecaseTests.swift
+++ VaionAddServerTests/AddServerUsecaseTests.swift
@@ -13,16 +13,7 @@ class AddServerUsecaseTests: XCTestCase {
     
     let networking = NetworkingMock()
     let ipAddressDefault = "127.0.0.1"
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
-    
+       
     func test_addServer_withIPAddressRequiresNoAuth_asksForNoCredentials() {
         let sut = createSUT()
         let expect = expectation(description: "noCredentials")
