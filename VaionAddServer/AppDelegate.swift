c791176c38a59e9d86ef8755824fa765b00ad06d
Murat Sudan <tarumnadus@gmail.com>
2020-03-09 08:52:42 +0000

    Refactoring uinavigationcontroller router extension

--- VaionAddServer/AppDelegate.swift
+++ VaionAddServer/AppDelegate.swift
@@ -8,21 +8,16 @@
 
 import UIKit
 
-extension UINavigationController: Router { }
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
     var flowAddServer: FlowAddServer?
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        
         let navigationController = UINavigationController()
         window?.rootViewController = navigationController
         
---
+++ VaionAddServer/Helpers/UINavigationController+extension.swift
@@ -0,0 +1,11 @@
+//
+//  UINavigationController+extension.swift
+//  VaionAddServer
+//
+//  Created by Murat Sudan on 9.03.2020.
+//  Copyright © 2020 Tarum Nadus. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController: Router { }
